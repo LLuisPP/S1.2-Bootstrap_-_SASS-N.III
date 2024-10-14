6d9dbb77777ed3228b0aadbc7b7aeb9e6f3df51b
Borealuis <borealuis@MacBook-Pro-de-Borealuis.local>
2024-10-14 21:44:18 +0200

    >+ | exercse 5 js validation script

---
+++ js/validation.js
@@ -0,0 +1,16 @@
+// generic validation from bootstrap web example
+
+    'use strict'
+  
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
